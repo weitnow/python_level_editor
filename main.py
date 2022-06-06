5db818087b660ce41fa27c5bc61366e33211ac7b
christian keusch <weitnow@gmail.com>
2022-06-06 12:13:45 +0200

    finished project

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import button
+import csv
+import pickle
 
 pygame.init()
 
@@ -47,7 +49,7 @@ GREEN = (144, 201, 120)
 WHITE = (255, 255, 255)
 RED = (200, 25, 25)
 
-#define font
+# define font
 font = pygame.font.SysFont('Futura', 30)
 
 # create empty tile list
@@ -60,7 +62,8 @@ for row in range(ROWS):
 for tile in range(MAX_COLS):
     world_data[ROWS - 1][tile] = 0
 
-#function for outputting text onto the screen
+
+# function for outputting text onto the screen
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
@@ -120,13 +123,15 @@ while run:
     if scroll_right == True and scroll < (MAX_COLS * TILE_SIZE) - SCREEN_WIDTH:
         scroll += 5 * scroll_speed
 
-
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         # keyboard presses
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                level += 1
+            if event.key == pygame.K_s and level > 0:
+                level -= 1
             if event.key == pygame.K_a:
                 scroll_left = True
             if event.key == pygame.K_d:
@@ -134,21 +139,20 @@ while run:
             if event.key == pygame.K_LSHIFT:
                 scroll_speed = 5
 
-        #add new tiles to the screen
-        #get mouse position
+        # add new tiles to the screen
+        # get mouse position
         pos = pygame.mouse.get_pos()
         x = (pos[0] + scroll) // TILE_SIZE
         y = pos[1] // TILE_SIZE
 
-        #check that the coordinates are within the tile area
+        # check that the coordinates are within the tile area
         if pos[0] < SCREEN_WIDTH and pos[1] < SCREEN_HEIGHT:
-            #update tile value
+            # update tile value
             if pygame.mouse.get_pressed()[0] == 1:
                 if world_data[y][x] != current_tile:
                     world_data[y][x] = current_tile
             if pygame.mouse.get_pressed()[2] == 1:
                 world_data[y][x] = -1
-
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
@@ -165,9 +169,24 @@ while run:
     draw_text(f'Level: {level}', font, WHITE, 10, SCREEN_HEIGHT + LOWER_MARGIN - 90)
     draw_text('Press W or S to change level', font, WHITE, 10, SCREEN_HEIGHT + LOWER_MARGIN - 60)
 
-    #save and load data
-    save_button.draw(screen)
-    load_button.draw(screen)
+    # save and load data
+    if save_button.draw(screen):
+        # save level data
+        with open(f'level{level}_data.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            for row in world_data:
+                writer.writerow(row)
+    if load_button.draw(screen):
+        #load in level data
+        #reset scroll back to the start of the level
+        scroll = 0
+        with open(f'level{level}_data.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for x, row in enumerate(reader):
+                for y, tile in enumerate(row):
+                    world_data[x][y] = int(tile)
+
+
 
     # draw tile panel an tiles
     pygame.draw.rect(screen, GREEN, (SCREEN_WIDTH, 0, SIDE_MARGIN, SCREEN_HEIGHT))
